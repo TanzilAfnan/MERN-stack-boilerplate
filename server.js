86e37b28804c9a56e163d49aca809aac29e3f9e7
Afnan Md.Tanzil <tanzil.afn@gmail.com>
2020-05-01 14:30:29 +0100

    This is it, Will update more later

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+const app = express();
+
+const port = 5000;
+
+app.get("/api/customers", (req, res) => {
+  const customers = [
+    { id: 1, firstName: "Bruce", lastName: "Wayne" },
+    { id: 2, firstName: "Clark", lastName: "Kent" },
+    { id: 3, firstName: "Wade", lastName: "Wilson" },
+    { id: 3, firstName: "Barry", lastName: "Allen" },
+  ];
+
+  res.json(customers);
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening for request @ port ${port}`);
+});
